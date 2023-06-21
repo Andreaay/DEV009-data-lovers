d4ddcfa10580359889dbd8950f4c20d0748162c1
Amalia Soto <75153007+amalia345@users.noreply.github.com>
2023-06-20 20:41:49 -0600

    ciclofor nombredirector

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-//import { example } from './data.js';
-// import data from './data/lol/lol.js';
-//import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 import data from './data/ghibli/ghibli.js';
-
-console.log(data, ghibli);
-
+//console.log(data.films[0].director);
+let listaPeliculasJs=document.querySelector("#lista")
+for (let i = 0; i < 20; i++) {
+    let texto = document.createTextNode("Director: "+data.films[i].director)
+    let nuevoLiFor = document.createElement('li'); 
+    nuevoLiFor.appendChild(texto)
+    listaPeliculasJs.appendChild(nuevoLiFor)
+}
 
