f5614d8b3bc08388706972bffc8825e513b85f58
Amalia Soto <75153007+amalia345@users.noreply.github.com>
2023-06-23 11:42:52 -0600

    funcion hayao

--- src/main.js
+++ src/main.js
@@ -3,18 +3,45 @@ import data from './data/ghibli/ghibli.js';
 let peliculasList = document.querySelector("#peliculas-list");
 let selectorTiempo = document.getElementById("tiempo")
 let selectorDirectores = document.getElementById("directores")
+let selectorHayao = document.getElementById("Hayao Miyazaki")
 
 viejas()
 
 document.getElementById("refrescar").addEventListener('click',limpiarPantalla)
 //funciones aqui abajo
 function directores() {
-  let directorSeleccionado=selectorDirectores.value 
-  limpiarPantalla()
-  alert("Acabas de seleccionar al director " +directorSeleccionado)
-  for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+  //let directorSeleccionado=selectorDirectores.value 
+  //limpiarPantalla()
+ let directorHayao=selectorDirectores.value
+ limpiarPantalla()
+
+
+  //alert("Acabas de seleccionar al director " +directorSeleccionado)
+  //for(let i =0; i< array.length; i++){
+   // let =array[i];
+//}
+  for (let i = 0; i < 20; i++) {
+    let pelicula = data.films[i];
+    let contenidoHTML = `
+      <div class="pelicula">
+      <section id= "section3" >
+      <img src="${pelicula.poster}" alt="${pelicula.title} poster" />
+      </section>
+      <h3>${pelicula.title}</h3>
+      <section id= "section2" >
+        <p><strong>Director:</strong> ${pelicula.director}</p>
+        <p><strong>Descripción:</strong> ${pelicula.description}</p>
+        </section>
+      </div>
+    `;
+    let peliculaItem = document.createElement('li');
+    peliculaItem.innerHTML = contenidoHTML;
+    hayaoList.appendChild(peliculaItem);
+    if (pelicula.director === "Hayao Miyasaki "){
+    peliculaItem.style.display="block";}
+    else { 
+    return peliculaItem.style.display="none";
+    }
   }
 }
 selectorDirectores.addEventListener('change',directores)
