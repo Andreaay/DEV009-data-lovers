bc400ef25612c92c55c21aadba19f5e558a80e63
Andrea <andreacabrera37@gmail.com>
2023-07-03 14:23:41 -0600

    arreglos generales

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,7 +1,6 @@
 import { filterDirector } from '../src/data.js';
 import { sortAlphabeticYear } from '../src/data.js';
 import { ordenarPorNumeros } from '../src/data.js'; 
-// import { mostrarPersonaje } from '.main.js';
 
 describe('Tests de sortAlphabeticYear', () => {
   it('Test para filtrar las peliculas de la A a la Z', () => {
@@ -2358,63 +2357,5 @@ describe('Tests para ordenarPorNumeros', () => {
     ]
     const peliculasfiltradas = ordenarPorNumeros (peliculas, 'peor');
     expect(peliculasfiltradas).toEqual(resultadoEsperado);
-  });
-});
-
-
-describe('Test para mostrarPersonaje', () => {
-  it('Test para validar mostrarPersonaje', () => {
-    const personaje = [
-      {
-        "name": "Pazu",
-        "img": "https://static.wikia.nocookie.net/studio-ghibli/images/8/8b/Pazu.jpg",
-        "gender": "Male",
-        "age": "13",
-        "specie": "Human"
-      },
-      {
-        "name": "Satsuki Kusakabe",
-        "img": "https://static.wikia.nocookie.net/studio-ghibli/images/f/f2/Satsuki_Kusakabe.jpg",
-        "gender": "Female",
-        "age": "11",
-        "specie": "Human"
-      },
-      {
-        "name": "Kiki",
-        "img": "https://static.wikia.nocookie.net/studio-ghibli/images/5/59/Kiki.jpg",
-        "gender": "Female",
-        "age": "13",
-        "eye_color": "Black",
-        "hair_color": "Brown",
-        "specie": "Witch"
-      }
-    ];
-    const resultadoEsperado = [
-      {
-        "name": "Pazu",
-        "img": "https://static.wikia.nocookie.net/studio-ghibli/images/8/8b/Pazu.jpg",
-        "gender": "Male",
-        "age": "13",
-        "specie": "Human"
-      },
-      {
-        "name": "Satsuki Kusakabe",
-        "img": "https://static.wikia.nocookie.net/studio-ghibli/images/f/f2/Satsuki_Kusakabe.jpg",
-        "gender": "Female",
-        "age": "11",
-        "specie": "Human"
-      },
-      {
-        "name": "Kiki",
-        "img": "https://static.wikia.nocookie.net/studio-ghibli/images/5/59/Kiki.jpg",
-        "gender": "Female",
-        "age": "13",
-        "eye_color": "Black",
-        "hair_color": "Brown",
-        "specie": "Witch"
-      }
-    ];
-    const resultadoObtenido = mostrarPersonaje(personaje);
-    expect(resultadoObtenido).toEqual(resultadoEsperado);
   });
 });
