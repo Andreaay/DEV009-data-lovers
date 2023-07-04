d0fdf2e8c6618e44134e7f86a9d0640cca217928
Amalia Soto <75153007+amalia345@users.noreply.github.com>
2023-07-03 22:09:06 -0600

    flex-box li

--- src/main.js
+++ src/main.js
@@ -1,30 +1,33 @@
 import data from './data/ghibli/ghibli.js';
 import { filterDirector, sortAlphabeticYear, ordenarPorNumeros, filterPoster } from "./data.js";
 const todasLasPeliculas = data.films;// la constante donde guardamos los 20 objetos de las peliculas
+
 const botonHome = document.getElementById("home-button")
 const botonPersonajes = document.getElementById("personajes-button")
-const peliculasList = document.querySelector("#peliculas-list");//Home
+const flyersButton = document.getElementById("flyers-Button")
+
 const selectorDirectores = document.getElementById("directores")
 const selectorTiempo = document.getElementById("tiempo")
 const selectorCalificacion = document.getElementById("calificaciones")//estaba mal el nombre del id
-const flyersButton = document.getElementById("flyers-Button")
+
+const peliculasList = document.querySelector("#peliculas-list");//Un ol que es un contener de nuestros li
 
 function mostrarPeliculas(parametro) {//parametro hace que sea reutilizable
     peliculasList.innerHTML = "";//Limpiador//Limpiador
-    for (let i = 0; i < parametro.length; i++) {
+    for (let i = 0; i < parametro.length; i++) {// parametro.lenght es el largo de la lista de pelis
         let pelicula = parametro[i];//Limpiador//Limpiador
         let contenidoHTML = `
     <div class="pelicula">
-    <section id= "section3" >
-    <img src="${pelicula.poster}" alt="${pelicula.title} poster" />
-    </section>
-    <h3>${pelicula.title}</h3>
-    <section id= "section2" >
-        <p><strong>Director:</strong> ${pelicula.director}</p>
-        <p><strong>Producer:</strong> ${pelicula.producer}</p>
-        <p><strong>Release date:</strong> ${pelicula.release_date}</p>
-        <p><strong>RT SCORE:</strong> ${pelicula.rt_score}</p>
-        <p><strong>Descripción:</strong> ${pelicula.description}</p>
+        <section id= "section3" >
+            <img src="${pelicula.poster}" alt="${pelicula.title} poster" />
+        </section>
+            <h3>${pelicula.title}</h3>
+        <section id= "section2" >
+            <p><strong>Director:</strong> ${pelicula.director}</p>
+            <p><strong>Producer:</strong> ${pelicula.producer}</p>
+            <p><strong>Release date:</strong> ${pelicula.release_date}</p>
+            <p><strong>RT SCORE:</strong> ${pelicula.rt_score}</p>
+            <p class="texto"><strong>Descripción:</strong> ${pelicula.description}</p>
         </section>
     </div>
     `;
@@ -34,7 +37,6 @@ function mostrarPeliculas(parametro) {//parametro hace que sea reutilizable
     }
 }
 mostrarPeliculas(todasLasPeliculas); // este es el carga al inciar la pagina, muestra las 20
-function mostrarPersonaje(parametro) {
 
 function mostrarPersonaje(parametro) {
     peliculasList.innerHTML = "";
@@ -54,56 +56,13 @@ function mostrarPersonaje(parametro) {
     </div>
     `;
         let peliculaItem = document.createElement('ul');
-        let peliculaItem = document.createElement('ul');
         peliculaItem.innerHTML = contenidoHTML;
         peliculasList.appendChild(peliculaItem);
         
     }
 }
+
 // Event Listeners
-botonHome.addEventListener('click', function () {//change lee si hay un clcik en home
-    mostrarPeliculas(todasLasPeliculas);
-})
-botonPersonajes.addEventListener('click', function () {//change lee si hay un clcik en home
-    mostrarPersonaje(todasLasPeliculas);
-})
-selectorDirectores.addEventListener('change', function () {//change lee si hay un cambio en el select
-    const directorEscogido = selectorDirectores.value//Se guarda el valor del select
-    if (directorEscogido === "directores") {
-        mostrarPeliculas(todasLasPeliculas);
-    }
-    else {
-        mostrarPeliculas(filterDirector(todasLasPeliculas, directorEscogido));
-    }
-})
-selectorTiempo.addEventListener('change', function () {
-    console.log("click")
-    const selectorEscogido = selectorTiempo.value
-    if (selectorEscogido === "all") {
-        mostrarPeliculas(todasLasPeliculas);
-    } else {
-        mostrarPeliculas(sortAlphabeticYear(todasLasPeliculas, selectorEscogido))
-    }
-    /* if (tiempoEscogido == "viejas") {
-         mostrarPeliculas((ordenarPorNumeros(todasLasPeliculas, "release_date",true)))//Sirve para indicarle asl sort si es ascendente en true
-     } else if (tiempoEscogido == "nuevas") {
-         mostrarPeliculas(( ordenarPorNumeros(todasLasPeliculas,"release_date",false)))//Desendente en false false slase false false false false
-     } else if (tiempoEscogido == "alfabeto") {
-         mostrarPeliculas(( ordenarPorString(todasLasPeliculas,"title",true)))
-     } else if (tiempoEscogido == "alfabetoReversa"){
-         mostrarPeliculas(( ordenarPorString(todasLasPeliculas,"title",false)))
-     }
-     }*/
-})
-selectorCalificacion.addEventListener('change', function () {
-    const calificacionEscogido = selectorCalificacion.value
-    if (calificacionEscogido == "mejor") {
-        mostrarPeliculas((ordenarPorNumeros(todasLasPeliculas, "rt_score", false)))//Sirve para indicarle asl sort si es ascendente en true
-    } else {
-        mostrarPeliculas((ordenarPorNumeros(todasLasPeliculas, "rt_score", true)))
-    }
-})
-//función para botón de posters
 flyersButton.addEventListener('click', function () {
     const posters = filterPoster(data);
     peliculasList.innerHTML = ""; // Limpiar la lista antes de mostrar los posters
@@ -114,7 +73,36 @@ flyersButton.addEventListener('click', function () {
         peliculasList.appendChild(img);
     }
 });
-// event listener que lea els elector de tiempo
-//constante donde guardar el valor
-// if selección viejo ordena por viejo
-//if selección nuevo ordena por nuevo
+
+botonHome.addEventListener('click', function () {//change lee si hay un clcik en home
+    mostrarPeliculas(todasLasPeliculas);
+})
+botonPersonajes.addEventListener('click', function () {//change lee si hay un clcik en home
+    mostrarPersonaje(todasLasPeliculas);
+})
+selectorDirectores.addEventListener('change', function () {//change lee si hay un cambio en el select
+    const directorEscogido = selectorDirectores.value//Se guarda el valor del select
+    if (directorEscogido === "directores") { // si el valor identico es directores
+        mostrarPeliculas(todasLasPeliculas);
+    }
+    else { // si no es directors, filtralo segun el nombre dle director
+        mostrarPeliculas(filterDirector(todasLasPeliculas, directorEscogido));
+    }
+})
+selectorTiempo.addEventListener('change', function () {
+    const selectorEscogido = selectorTiempo.value
+    if (selectorEscogido === "sort") { // si no escoge una opcion en especifico MUESTRA TODAS las peliculas
+        mostrarPeliculas(todasLasPeliculas);
+    } else {
+        mostrarPeliculas(sortAlphabeticYear(todasLasPeliculas, selectorEscogido))
+    }
+})
+selectorCalificacion.addEventListener('change', function () {
+    const calificacionEscogido = selectorCalificacion.value
+    if (calificacionEscogido == "mejor") {
+        mostrarPeliculas((ordenarPorNumeros(todasLasPeliculas, "rt_score", false)))//Sirve para indicarle asl sort si es ascendente en true
+    } else {
+        mostrarPeliculas((ordenarPorNumeros(todasLasPeliculas, "rt_score", true)))
+    }
+})
+//función para botón de posters
