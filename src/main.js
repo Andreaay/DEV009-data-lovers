49abb5329b7f33e9fe3c000291df8babbaa6583e
Andrea <andreacabrera37@gmail.com>
2023-07-04 21:00:32 -0600

    acomodo de código

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import data from './data/ghibli/ghibli.js';
 import { filterDirector, sortAlphabeticYear, ordenarPorNumeros, filterPoster } from "./data.js";
+
 const todasLasPeliculas = data.films;// la constante donde guardamos los 20 objetos de las peliculas
 const botonHome = document.getElementById("home-button")
 const botonPersonajes = document.getElementById("personajes-button")
@@ -8,6 +9,7 @@ const selectorDirectores = document.getElementById("directores")
 const selectorTiempo = document.getElementById("tiempo")
 const selectorCalificacion = document.getElementById("calificaciones")//estaba mal el nombre del id
 const flyersButton = document.getElementById("flyers-Button")
+
 function mostrarPeliculas(parametro) {//parametro hace que sea reutilizable
   peliculasList.innerHTML = "";//Limpiador
   for (let i = 0; i < parametro.length; i++) {
@@ -32,35 +34,10 @@ function mostrarPeliculas(parametro) {//parametro hace que sea reutilizable
     peliculasList.appendChild(peliculaItem);//Esta parte mueve el li dentro del ol creado en HTML
   }
 }
-mostrarPeliculas(todasLasPeliculas); // este es el carga al inciar la pagina, muestra las 20
-function mostrarPersonaje(parametro) {
-  peliculasList.innerHTML = "";
-  for (let i = 0; i < parametro.length; i++) {
-    const pelicula = parametro[i];
-    const contenidoHTML = `
-    <div class="mostrarPersonaje">
-    <section id= "section3" >
-    <img src="${pelicula.people[0].img}" alt="${pelicula.people[0].name} poster" />
-    </section>
-    <h3>${pelicula.people[0].name}</h3>
-    <section id= "section2" >
-        <p><strong>Age:</strong> ${pelicula.people[0].age}</p>
-        <p><strong>Gender:</strong> ${pelicula.people[0].gender}</p>
-        <p><strong>Species:</strong> ${pelicula.people[0].specie}</p>
-        </section>
-    </div>
-    `;
-    const peliculaItem = document.createElement('ul'); //se utiliza ul para poder identificarlo en CSS
-    peliculaItem.innerHTML = contenidoHTML;
-    peliculasList.appendChild(peliculaItem);
-  }
-}
+
 // Event Listeners
-botonHome.addEventListener('click', function () {//change lee si hay un clcik en home
+botonHome.addEventListener('click', function () {//change lee si hay un click en home
   mostrarPeliculas(todasLasPeliculas);
-})
-botonPersonajes.addEventListener('click', function () {//change lee si hay un clcik en home
-  mostrarPersonaje(todasLasPeliculas);
 })
 selectorDirectores.addEventListener('change', function () {//change lee si hay un cambio en el select
   const directorEscogido = selectorDirectores.value//Se guarda el valor del select
@@ -88,6 +65,10 @@ selectorCalificacion.addEventListener('change', function () {
     mostrarPeliculas((ordenarPorNumeros(todasLasPeliculas, "rt_score", true)))
   }
 })
+botonPersonajes.addEventListener('click', function () {//change lee si hay un click en home
+  mostrarPersonaje(todasLasPeliculas);
+})
+
 //función para botón de posters
 flyersButton.addEventListener('click', function () {
   const posters = filterPoster(data);
@@ -99,3 +80,27 @@ flyersButton.addEventListener('click', function () {
     peliculasList.appendChild(img);
   }
 });
+
+mostrarPeliculas(todasLasPeliculas); // este es el carga al inciar la pagina, muestra las 20
+function mostrarPersonaje(parametro) {
+  peliculasList.innerHTML = "";
+  for (let i = 0; i < parametro.length; i++) {
+    const pelicula = parametro[i];
+    const contenidoHTML = `
+    <div class="mostrarPersonaje">
+    <section id= "section3" >
+    <img src="${pelicula.people[0].img}" alt="${pelicula.people[0].name} poster" />
+    </section>
+    <h3>${pelicula.people[0].name}</h3>
+    <section id= "section2" >
+        <p><strong>Age:</strong> ${pelicula.people[0].age}</p>
+        <p><strong>Gender:</strong> ${pelicula.people[0].gender}</p>
+        <p><strong>Species:</strong> ${pelicula.people[0].specie}</p>
+        </section>
+    </div>
+    `;
+    const peliculaItem = document.createElement('ul'); //se utiliza ul para poder identificarlo en CSS
+    peliculaItem.innerHTML = contenidoHTML;
+    peliculasList.appendChild(peliculaItem);
+  }
+}
