68c8a7a8af912093b54e517e77fcb8e4aee27811
Amalia Soto <75153007+amalia345@users.noreply.github.com>
2023-07-05 23:21:07 -0600

    Characters

--- src/data.js
+++ src/data.js
@@ -25,8 +25,9 @@ export const ordenarPorNumeros = (listaPeliculas,campoOrdenar,direccion) => {//l
   return listaPeliculas.sort((a,b) => {
     if (parseInt(a[campoOrdenar])< parseInt(b[campoOrdenar])) {
       return direccion ? -1 : 1;// signo interrogacion con dos puntos es un iif else miniatura se llama ternario
+      //si a es menor que b, coloca a antes que b
     } else if (parseInt(a[campoOrdenar])>parseInt( b[campoOrdenar])) {
-      return direccion ? 1 : -1;
+      return direccion ? 1 : -1;//
     }
     return 0;
   });
@@ -35,4 +36,16 @@ export const filterPoster = (data) => {
   const posters = data.films.filter(pelicula => pelicula.poster !== null && pelicula.poster !== undefined);
   return posters.map(pelicula => pelicula.poster);
 };
+
   
+export const averageCharacters = (film) => {
+  return (film.people.length);
+}
+export const averageCharactersAllFilms = (films) => {
+  let totalPersonajes=0;
+  for (let i = 0; i < films.length; i++) {
+    totalPersonajes = totalPersonajes + films[i].people.length
+  }
+  return (totalPersonajes/films.length);
+}
+
--- src/main.js
+++ src/main.js
@@ -1,27 +1,28 @@
 import data from './data/ghibli/ghibli.js';
-import { filterDirector, sortAlphabeticYear, ordenarPorNumeros, filterPoster } from "./data.js";
+import { filterDirector, sortAlphabeticYear, ordenarPorNumeros, filterPoster, averageCharacters,averageCharactersAllFilms } from "./data.js";
+
 
 const todasLasPeliculas = data.films;// la constante donde guardamos los 20 objetos de las peliculas
 const botonHome = document.getElementById("home-button")
 const botonPersonajes = document.getElementById("personajes-button")
-const peliculasList = document.querySelector("#peliculas-list");//Home
+const peliculasList = document.querySelector("#peliculas-list");//Home OL
 const selectorDirectores = document.getElementById("directores")
 const selectorTiempo = document.getElementById("tiempo")
 const selectorCalificacion = document.getElementById("calificaciones")//estaba mal el nombre del id
 const flyersButton = document.getElementById("flyers-Button")
 
 function mostrarPeliculas(parametro) {//parametro hace que sea reutilizable
-  peliculasList.innerHTML = "";//Limpiador
-  for (let i = 0; i < parametro.length; i++) {
-    const pelicula = parametro[i];
-    const contenidoHTML = `
+    peliculasList.innerHTML = "";//Limpiador
+    for (let i = 0; i < parametro.length; i++) { //Intera la información de todas las peliculas
+        const pelicula = parametro[i]; //{$} se utiliza para insertar expresiones dentro de una cadena de texto
+        const contenidoHTML = ` 
     <div class="pelicula">
     <section id= "section3" >
     <img src="${pelicula.poster}" alt="${pelicula.title} poster" />
     </section>
     <h3>${pelicula.title}</h3>
     <section id= "section2" >
-        <p><strong>Director:</strong> ${pelicula.director}</p>
+        <p><strong>Director:</strong> ${pelicula.director}</p> 
         <p><strong>Producer:</strong> ${pelicula.producer}</p>
         <p><strong>Release date:</strong> ${pelicula.release_date}</p>
         <p><strong>RT SCORE:</strong> ${pelicula.rt_score}</p>
@@ -29,91 +30,66 @@ function mostrarPeliculas(parametro) {//parametro hace que sea reutilizable
         </section>
     </div>
     `;
-    const peliculaItem = document.createElement('li');
-    peliculaItem.innerHTML = contenidoHTML;
-    peliculasList.appendChild(peliculaItem);//Esta parte mueve el li dentro del ol creado en HTML
-  }
+        const peliculaItem = document.createElement('li');
+        peliculaItem.innerHTML = contenidoHTML;
+        peliculasList.appendChild(peliculaItem);//Esta parte mueve el li dentro del ol creado en HTML
+    }
 }
-<<<<<<< HEAD
-mostrarPeliculas(todasLasPeliculas); // este es el carga al inciar la pagina, muestra las 20
-function mostrarPersonaje(parametro) {
-  peliculasList.innerHTML = "";
-  for (let i = 0; i < parametro.length; i++) {
-    const pelicula = parametro[i];
-    const contenidoHTML = `
-    <div class="mostrarPersonaje">
-    <section id= "section3" >
-    <img src="${pelicula.people[0].img}" alt="${pelicula.people[0].name} poster" />
-    </section>
-    <h3>${pelicula.people[0].name}</h3>
-    <section id= "section2" >
-        <p><strong>Age:</strong> ${pelicula.people[0].age}</p>
-        <p><strong>Gender:</strong> ${pelicula.people[0].gender}</p>
-        <p><strong>Species:</strong> ${pelicula.people[0].specie}</p>
-        </section>
-    </div>
-    `;
-    const peliculaItem = document.createElement('li'); //se utiliza ul para poder identificarlo en CSS
-    peliculaItem.innerHTML = contenidoHTML;
-    peliculasList.appendChild(peliculaItem);
-    
-  }
-}
-=======
-
->>>>>>> e1301e0955578841ac06425a54868dc9cb9f26b6
 // Event Listeners
 botonHome.addEventListener('click', function () {//change lee si hay un click en home
-  mostrarPeliculas(todasLasPeliculas);
+    mostrarPeliculas(todasLasPeliculas);
 })
 selectorDirectores.addEventListener('change', function () {//change lee si hay un cambio en el select
-  const directorEscogido = selectorDirectores.value//Se guarda el valor del select
-  if (directorEscogido === "directores") {
-    mostrarPeliculas(todasLasPeliculas);
-  }
-  else {
-    mostrarPeliculas(filterDirector(todasLasPeliculas, directorEscogido));
-  }
+    const directorEscogido = selectorDirectores.value//Se guarda el valor del select
+    if (directorEscogido === "directores") {
+        mostrarPeliculas(todasLasPeliculas);
+    }
+    else {
+        mostrarPeliculas(filterDirector(todasLasPeliculas, directorEscogido));
+    }
 })
 selectorTiempo.addEventListener('change', function () {
-  console.log("click")
-  const selectorEscogido = selectorTiempo.value
-  if (selectorEscogido === "all") {
-    mostrarPeliculas(todasLasPeliculas);
-  } else {
-    mostrarPeliculas(sortAlphabeticYear(todasLasPeliculas, selectorEscogido))
-  }
+    const selectorEscogido = selectorTiempo.value
+    if (selectorEscogido === "all") { //Igualdad estricta
+        mostrarPeliculas(todasLasPeliculas);//tombola
+    } else {
+        mostrarPeliculas(sortAlphabeticYear(todasLasPeliculas, selectorEscogido))//clasifica
+    }
 })
 selectorCalificacion.addEventListener('change', function () {
-  const calificacionEscogido = selectorCalificacion.value
-  if (calificacionEscogido === "mejor") {
-    mostrarPeliculas((ordenarPorNumeros(todasLasPeliculas, "rt_score", false)))//Sirve para indicarle asl sort si es ascendente en true
-  } else {
-    mostrarPeliculas((ordenarPorNumeros(todasLasPeliculas, "rt_score", true)))
-  }
+    const calificacionEscogido = selectorCalificacion.value
+    if (calificacionEscogido === "mejor") {
+        mostrarPeliculas((ordenarPorNumeros(todasLasPeliculas, "rt_score", false)))//Sirve para indicarle asl sort si es ascendente en true
+    } else {
+        mostrarPeliculas((ordenarPorNumeros(todasLasPeliculas, "rt_score", true)))
+    }
 })
 botonPersonajes.addEventListener('click', function () {//change lee si hay un click en home
-  mostrarPersonaje(todasLasPeliculas);
+    mostrarPersonaje(todasLasPeliculas);
 })
 
 //función para botón de posters
 flyersButton.addEventListener('click', function () {
-  const posters = filterPoster(data);
-  peliculasList.innerHTML = ""; // Limpiar la lista antes de mostrar los posters
-  for (let i = 0; i < posters.length; i++) {
-    const img = document.createElement('img');
-    img.src = posters[i];
-    img.classList.add('flyer')
-    peliculasList.appendChild(img);
-  }
+    const posters = filterPoster(data);
+    peliculasList.innerHTML = ""; // Limpiar la lista antes de mostrar los posters
+    for (let i = 0; i < posters.length; i++) {
+        const img = document.createElement('img');
+        img.src = posters[i];
+        img.classList.add('flyer')
+        peliculasList.appendChild(img);
+    }
 });
 
 mostrarPeliculas(todasLasPeliculas); // este es el carga al inciar la pagina, muestra las 20
+
 function mostrarPersonaje(parametro) {
-  peliculasList.innerHTML = "";
-  for (let i = 0; i < parametro.length; i++) {
-    const pelicula = parametro[i];
-    const contenidoHTML = `
+    peliculasList.innerHTML = "";
+    let totalCharacters = 0;
+    for (let i = 0; i < parametro.length; i++) {
+        
+        totalCharacters = totalCharacters + averageCharacters(parametro[i])
+        const pelicula = parametro[i];
+        const contenidoHTML = `
     <div class="mostrarPersonaje">
     <section id= "section3" >
     <img src="${pelicula.people[0].img}" alt="${pelicula.people[0].name} poster" />
@@ -123,12 +99,15 @@ function mostrarPersonaje(parametro) {
         <p><strong>Age:</strong> ${pelicula.people[0].age}</p>
         <p><strong>Gender:</strong> ${pelicula.people[0].gender}</p>
         <p><strong>Species:</strong> ${pelicula.people[0].specie}</p>
-        </section>
+        <p><strong>Numero de personajes en la pelicula:</strong> ${averageCharacters(parametro[i])}</p>
+        <p><strong>Personajes promedio de Studio Ghibli:</strong> ${averageCharactersAllFilms(parametro)}</p>
+        </section> 
     </div>
     `;
-    const peliculaItem = document.createElement('ul'); //se utiliza ul para poder identificarlo en CSS
-    peliculaItem.classList.add('mainCharacters');
-    peliculaItem.innerHTML = contenidoHTML;
-    peliculasList.appendChild(peliculaItem);
-  }
+        const peliculaItem = document.createElement('ul'); //se utiliza ul para poder identificarlo en CSS
+        peliculaItem.classList.add('mainCharacters');
+        peliculaItem.innerHTML = contenidoHTML;
+        peliculasList.appendChild(peliculaItem);
+    }
+    
 }
