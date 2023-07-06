18da9fcf183d6ae35f25297bb30e1cca7cad5ac9
Andrea <andreacabrera37@gmail.com>
2023-07-06 09:21:29 -0600

    tests listos

--- src/main.js
+++ src/main.js
@@ -12,10 +12,10 @@ const selectorCalificacion = document.getElementById("calificaciones")//estaba m
 const flyersButton = document.getElementById("flyers-Button")
 
 function mostrarPeliculas(parametro) {//parametro hace que sea reutilizable
-    peliculasList.innerHTML = "";//Limpiador
-    for (let i = 0; i < parametro.length; i++) { //Intera la información de todas las peliculas
-        const pelicula = parametro[i]; //{$} se utiliza para insertar expresiones dentro de una cadena de texto
-        const contenidoHTML = ` 
+  peliculasList.innerHTML = "";//Limpiador
+  for (let i = 0; i < parametro.length; i++) { //Intera la información de todas las peliculas
+    const pelicula = parametro[i]; //{$} se utiliza para insertar expresiones dentro de una cadena de texto
+    const contenidoHTML = ` 
     <div class="pelicula">
     <section id= "section3" >
     <img src="${pelicula.poster}" alt="${pelicula.title} poster" />
@@ -30,68 +30,69 @@ function mostrarPeliculas(parametro) {//parametro hace que sea reutilizable
         </section>
     </div>
     `;
-        const peliculaItem = document.createElement('li');
-        peliculaItem.innerHTML = contenidoHTML;
-        peliculasList.appendChild(peliculaItem);//Esta parte mueve el li dentro del ol creado en HTML
-    }
+    const peliculaItem = document.createElement('li');
+    peliculaItem.innerHTML = contenidoHTML;
+    peliculasList.appendChild(peliculaItem);//Esta parte mueve el li dentro del ol creado en HTML
+  }
 }
 // Event Listeners
 botonHome.addEventListener('click', function () {//change lee si hay un click en home
-    mostrarPeliculas(todasLasPeliculas);
+  mostrarPeliculas(todasLasPeliculas);
 })
 selectorDirectores.addEventListener('change', function () {//change lee si hay un cambio en el select
-    const directorEscogido = selectorDirectores.value//Se guarda el valor del select
-    if (directorEscogido === "directores") {
-        mostrarPeliculas(todasLasPeliculas);
-    }
-    else {
-        mostrarPeliculas(filterDirector(todasLasPeliculas, directorEscogido));
-    }
+  const directorEscogido = selectorDirectores.value//Se guarda el valor del select
+  if (directorEscogido === "directores") {
+    mostrarPeliculas(todasLasPeliculas);
+  }
+  else {
+    mostrarPeliculas(filterDirector(todasLasPeliculas, directorEscogido));
+  }
 })
 selectorTiempo.addEventListener('change', function () {
-    const selectorEscogido = selectorTiempo.value
-    if (selectorEscogido === "all") { //Igualdad estricta
-        mostrarPeliculas(todasLasPeliculas);//tombola
-    } else {
-        mostrarPeliculas(sortAlphabeticYear(todasLasPeliculas, selectorEscogido))//clasifica
-    }
+  const selectorEscogido = selectorTiempo.value
+  if (selectorEscogido === "all") { //Igualdad estricta
+    mostrarPeliculas(todasLasPeliculas);//tombola
+  } else {
+    mostrarPeliculas(sortAlphabeticYear(todasLasPeliculas, selectorEscogido))//clasifica
+  }
 })
 selectorCalificacion.addEventListener('change', function () {
-    const calificacionEscogido = selectorCalificacion.value
-    if (calificacionEscogido === "mejor") {
-        mostrarPeliculas((ordenarPorNumeros(todasLasPeliculas, "rt_score", false)))//Sirve para indicarle asl sort si es ascendente en true
-    } else {
-        mostrarPeliculas((ordenarPorNumeros(todasLasPeliculas, "rt_score", true)))
-    }
+  const calificacionEscogido = selectorCalificacion.value
+  if (calificacionEscogido === "mejor") {
+    mostrarPeliculas((ordenarPorNumeros(todasLasPeliculas, "rt_score", false)))//Sirve para indicarle asl sort si es ascendente en true
+  } else {
+    mostrarPeliculas((ordenarPorNumeros(todasLasPeliculas, "rt_score", true)))
+  }
 })
 botonPersonajes.addEventListener('click', function () {//change lee si hay un click en home
-    mostrarPersonaje(todasLasPeliculas);
+  mostrarPersonaje(todasLasPeliculas);
 })
 
 //función para botón de posters
 flyersButton.addEventListener('click', function () {
-    const posters = filterPoster(data);
-    peliculasList.innerHTML = ""; // Limpiar la lista antes de mostrar los posters
-    for (let i = 0; i < posters.length; i++) {
-        const img = document.createElement('img');
-        img.src = posters[i];
-        img.classList.add('flyer')
-        peliculasList.appendChild(img);
-    }
+  const posters = filterPoster(data);
+  peliculasList.innerHTML = ""; // Limpiar la lista antes de mostrar los posters
+  for (let i = 0; i < posters.length; i++) {
+    const img = document.createElement('img');
+    img.src = posters[i];
+    img.classList.add('flyer')
+    peliculasList.appendChild(img);
+  }
 });
 
 mostrarPeliculas(todasLasPeliculas); // este es el carga al inciar la pagina, muestra las 20
 
 function mostrarPersonaje(parametro) {
-    peliculasList.innerHTML = "";
-    let totalCharacters = 0;
-    for (let i = 0; i < parametro.length; i++) {
+  peliculasList.innerHTML = "";
+  let totalCharacters = 0;
+  for (let i = 0; i < parametro.length; i++) {
         
-        totalCharacters = totalCharacters + averageCharacters(parametro[i])
-        const pelicula = parametro[i];
-        const contenidoHTML = `
+    totalCharacters = totalCharacters + averageCharacters(parametro[i])
+    const pelicula = parametro[i];
+    const contenidoHTML = `
     <div class="mostrarPersonaje">
     <section id= "section3" >
+    <h3><strong>Title:</strong> ${pelicula.title}</h3>
     <img src="${pelicula.people[0].img}" alt="${pelicula.people[0].name} poster" />
     </section>
     <h3>${pelicula.people[0].name}</h3>
@@ -99,15 +100,14 @@ function mostrarPersonaje(parametro) {
         <p><strong>Age:</strong> ${pelicula.people[0].age}</p>
         <p><strong>Gender:</strong> ${pelicula.people[0].gender}</p>
         <p><strong>Species:</strong> ${pelicula.people[0].specie}</p>
-        <p><strong>Numero de personajes en la pelicula:</strong> ${averageCharacters(parametro[i])}</p>
-        <p><strong>Personajes promedio de Studio Ghibli:</strong> ${averageCharactersAllFilms(parametro)}</p>
+        <p><strong>Number of characters in the movie:</strong> ${averageCharacters(parametro[i])}</p>
+        <p><strong>Overall average of characters in Studio Ghibli movies: ${averageCharactersAllFilms(parametro)}</p>
         </section> 
     </div>
     `;
-        const peliculaItem = document.createElement('ul'); //se utiliza ul para poder identificarlo en CSS
-        peliculaItem.classList.add('mainCharacters');
-        peliculaItem.innerHTML = contenidoHTML;
-        peliculasList.appendChild(peliculaItem);
-    }
-    
+    const peliculaItem = document.createElement('ul'); //se utiliza ul para poder identificarlo en CSS
+    peliculaItem.classList.add('mainCharacters');
+    peliculaItem.innerHTML = contenidoHTML;
+    peliculasList.appendChild(peliculaItem);
+  }
 }
--- test/filter.spec.js
+++ test/filter.spec.js
@@ -1,4 +1,6 @@
 import { filterPoster } from '../src/data.js'; 
+import { averageCharacters } from '../src/data.js';
+import { averageCharactersAllFilms } from '../src/data.js';
 
 describe('Test de filterPoster', () => {
   it('Test para filtrar sólo el poster de cada película', () => {
@@ -26,4 +28,22 @@ describe('Test de filterPoster', () => {
     expect(posters[0]).toBe('https://static.wikia.nocookie.net/studio-ghibli/images/c/c1/Castle_in_the_Sky.jpg')
     expect(posters[0]).not.toBe('https://static.wikia.nocookie.net/studio-ghibli/images/4/48/Kiki%27s_Delivery_Service_%282%29.jpg')
   });
+});
+
+test('Test de averageCharacters que retorna la cantidad correcta de personajes por película', () => {
+  const film = {
+    people: ['"name": "Pazu"', '"name": "Satsuki Kusakabe"', '"name": "Kiki"']
+  };
+  const result = averageCharacters(film);
+  expect(result).toBe(3);
+});
+
+test('averageCharactersAllFilms retorna el promedio correcto de personajes en todas las películas', () => {
+  const films = [
+    { people: ['"name": "Pazu"', '"name": "Satsuki Kusakabe"'] },
+    { people: ['"name": "Kiki"', '"name": "Seita Yokokawa"', '"name": "Taeko Okajima"'] },
+    { people: ['"name": "Porco Rosso", "name": "Donald Curtis"'] }
+  ];
+  const result = averageCharactersAllFilms(films);
+  expect(result).toBeCloseTo(2, 2.30, 8.55);
 });
